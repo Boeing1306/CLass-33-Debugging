4d1502b7679ae4436806432dd938d48ca193e957
Jonathan <jonathansewsankar9@gmail.com>
2021-01-19 21:57:44 -0500

    Commented console.log

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.traj = [];
   }
 
   display() {
@@ -9,5 +11,18 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+
+    var position = [this.body.position.x,this.body.position.y];
+    this.traj.push(position);
+    //console.log(this.traj);
+
+    // this.traj = [[100,200],[110,220],[130,230],[140,250]]
+
+    for(var i = 0; i<this.traj.length; i++){
+      image(this.smokeImage,this.traj[i][0], this.traj[i][1] );
+      // this.traj[0][0], this.traj[0][1];
+      //this.traj[1][0], this.traj[1][1];
+      // this.traj[2][0], this.traj[2][1];
+    }
   }
 }
